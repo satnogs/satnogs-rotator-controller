03abf6fb0974dc3ffd98c35176e23e49a745d886
Agis Zisimatos <azisi@windowslive.com>
2014-11-11 20:48:41 +0200

    Add arduino code to control stepper motors and implement Easycom2 protocol

---
+++ Arduino/Stepper.ino
@@ -0,0 +1,49 @@
+/* Send pulses to stepper motor drivers */
+void stepper_move()
+{
+  if(stepAz>0)
+  {
+    digitalWrite(DIR_AZ, HIGH);
+    digitalWrite(STEP_AZ, HIGH); 
+    stepPosAz++;
+    stepAz--;
+  }
+  else if(stepAz<0)
+  {
+    digitalWrite(DIR_AZ, LOW);
+    digitalWrite(STEP_AZ, HIGH);
+    stepPosAz--;
+    stepAz++;
+  }
+
+  if(stepEl>0)
+  {
+    digitalWrite(DIR_EL, LOW);
+    digitalWrite(STEP_EL, HIGH); 
+    stepPosEl++;
+    stepEl--;
+  }
+  else if(stepEl<0)
+  {
+    digitalWrite(DIR_EL, HIGH);
+    digitalWrite(STEP_EL, HIGH);
+    stepPosEl--;
+    stepEl++;
+  }
+  
+  delay(T_STEPPER);
+  digitalWrite(STEP_AZ, LOW);
+  digitalWrite(STEP_EL, LOW);
+  delay(T_STEPPER);
+
+}
+/* Convert degrees to steps */
+int deg2step(double deg)
+{
+  return(60*SPR*deg/360);
+}
+/* Convert steps to degrees */
+float step2deg(int Step)
+{
+  return(360*Step/(SPR*60));
+}
--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 SatNOGS Arduino
 ===============
 
-Repository of Arduino code and Arduino PCB for SatNOGS project.
+Repository of Arduino code for SatNOGS project.
 Refer to block diagram in the repository for wiring documentation.
 
-The PCB is based in [Arduino Pro Micro](https://www.sparkfun.com/products/12640) and the [Pololu stepper motor driver](http://www.pololu.com/product/1182). In the picture below you can see the PCB.
-
-![satnogs_PCB](https://raw.github.com/satnogs/satnogs-arduino/master/Pics/SatNOGS_Board_V1_1.png)
+![satnogs_Diagram](https://raw.github.com/satnogs/satnogs-arduino/master/Pics/Block Diagram.png)
